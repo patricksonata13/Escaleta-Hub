a0936b8c5ec7d91eb26be8e2dbb8ebb8e99129e0
patricksonata13 <patrick12reis@gmail.com>
2025-09-25 10:47:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,43 @@ if (typeof firebase !== 'undefined') {
     firebase.initializeApp(firebaseConfig);
     console.log('Firebase inicializado');
 }
+// Sistema de formatação automática de roteiro
+class ScriptFormatter {
+    formatSceneHeading(text) {
+        return text.toUpperCase().replace(/(INT\.|EXT\.)\s+(.+)\s+-\s+(DIA|NOITE)/, 
+            '<div class="scene-heading">$&</div>');
+    }
+    
+    formatCharacterName(text) {
+        return text.replace(/^[A-Z\s]+$/, '<div class="character-name">$&</div>');
+    }
+}
+
+// Gerenciador de cenas
+class SceneManager {
+    constructor() {
+        this.scenes = [];
+        this.currentScene = 0;
+    }
+    
+    addScene(location, description) {
+        const scene = {
+            number: this.scenes.length + 1,
+            location,
+            description,
+            characters: []
+        };
+        this.scenes.push(scene);
+        this.updateTimeline();
+    }
+    
+    updateTimeline() {
+        const timeline = document.getElementById('scenesList');
+        timeline.innerHTML = this.scenes.map(scene => `
+            <div class="scene-item">
+                <strong>${scene.number}. ${scene.location}</strong>
+                <p>${scene.description}</p>
+            </div>
+        `).join('');
+    }
+}
